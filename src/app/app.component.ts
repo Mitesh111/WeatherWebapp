fc8dbadd1bb531ba0ae03604ad93ead062967140
Mitesh Kapasia <mitesh.kapasia@nagarro.com>
2019-04-19 14:53:51 +0530

    my commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {WeatherService} from 'src/app/weather.service'
+import {Weather} from 'src/app/weatherMetaData'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'weatherApp';
-}
+  city:string="";
+  a:boolean=false;
+  constructor(private weatherData: WeatherService){}
+  weatherOb:Weather[]=[];
+  getWeather(){
+    return this.weatherData.getWeather(this.city).subscribe((data:any)=>{
+      console.log(data);
+      for(let i=0;i<data.list.length;i=i+8){
+        const temp = new Weather(data.list[i].dt_txt,
+        data.city.name,
+        data.list[i].main.temp,
+        data.list[i].weather[0].icon,
+        data.list[i].weather[0].description,
+        data.list[i].main.temp_max,
+        data.list[i].main.temp_min,
+        data.list[i].main.pressure,
+        data.list[i].main.humidity,
+        data.list[i].wind.speed,
+        data.city.country)
+        this.weatherOb[i/8] = temp 
+        this.a=true
+        }   
+      }),
+      (error)=>{
+        console.log("City Not found")
+        console.log(error)
+      }
+    }
+  }
